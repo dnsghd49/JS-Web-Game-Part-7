0d1c9ce01ae9fc21de376d994a1746c03dec77f9
Aaron Lu <aaronnoh0409@gmail.com>
2021-11-10 16:46:18 -0500

    things that I did in class

--- index.js
+++ index.js
@@ -5,9 +5,17 @@ const pc = newPlayableCharacter(100, 110)
 const npc = newNonPlayableCharacter(50, 300)
 
 // have the NPC start walking east immediately
-npc.walkEast()
+async function mobie() {
+    await npc.walkEast(2000)
+    await npc.walkNorth(2000)
+    await npc.walkWest(2000)
+    await npc.walkSouth(2000)
+    mobie()
+}
 
-// Create the inventory
+mobie()
+
+//  the inventory
 const inventory = newInventory()
 move(inventory).to(0, 0)
 
